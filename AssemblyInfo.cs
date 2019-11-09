9ce687c49a7751782a805d4acd31d6fa4f51c89f
r4dius <r4dius@gmail.com>
2019-11-09 01:35:00 +0100

    Update

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("5.4")]
+[assembly: AssemblyVersion("5.5")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
@@ -56,4 +56,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
-[assembly: AssemblyFileVersionAttribute("5.4")]
+[assembly: AssemblyFileVersionAttribute("5.5")]
